3ffc98d7b54c0298df3fb492df6bfd7c554cff3e
mounishvatti <mounishvatti2002@gmail.com>
2024-09-24 22:36:34 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Coursera</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
