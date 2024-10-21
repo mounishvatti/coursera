27eb0b937d30922850e90ce94b7406c8a9e578d3
mounishvatti <mounishvatti2002@gmail.com>
2024-10-21 20:49:19 +0530

    etc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Coursera</h1>
-      </div>
     </>
   )
 }
